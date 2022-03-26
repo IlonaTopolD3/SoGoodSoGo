d554521dd23536deca9241b6f359144d7cfb68b4
Adam-Pacholski <adam.pacholski1985@gmail.com>
2022-03-26 13:31:54 +0100

    New componentsy.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegisterPageComponent } from './user/register-page/register-page.component';
+import { LoginPageComponent } from './user/login-page/login-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterPageComponent,
+    LoginPageComponent
   ],
   imports: [
     BrowserModule,
