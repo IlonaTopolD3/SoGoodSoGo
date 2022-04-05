a0177c067f7f0949627198d83c267776a02c0276
Adam-Pacholski <adam.pacholski1985@gmail.com>
2022-04-05 20:53:35 +0200

    poprawdki

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardPageComponent } from './dashboard/dashboard-page/dashboard-page.component';
 import { LoginPageComponent } from './user/login-page/login-page.component';
+import { RegisterPageComponent } from './user/register-page/register-page.component';
 
 const routes: Routes = [
   { path: '', component: LoginPageComponent},
-  { path: 'dashboard', component: DashboardPageComponent}
+  { path: 'dashboard', component: DashboardPageComponent},
+  { path: 'register',component: RegisterPageComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,15 @@ import { RegisterPageComponent } from './user/register-page/register-page.compon
 import { LoginPageComponent } from './user/login-page/login-page.component';
 import { FormsModule } from '@angular/forms';
 import { DashboardPageComponent } from './dashboard/dashboard-page/dashboard-page.component';
+import { NavPageComponent } from './dashboard/nav-page/nav-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     RegisterPageComponent,
     LoginPageComponent,
-    DashboardPageComponent
+    DashboardPageComponent,
+    NavPageComponent
   ],
   imports: [
     BrowserModule,
